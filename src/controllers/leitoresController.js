bbb564287a4c54bdc09e5c2cc31cb48982a06b6e
TairineEllen <tairine.ellen@gmail.com>
2020-12-07 19:45:40 -0300

    Função registerNewReader refatorada

--- src/controllers/leitoresController.js
+++ src/controllers/leitoresController.js
@@ -1,48 +1,37 @@
-const { leitoresModel } = require('../models/leitores');
-const { livrosModel } = require('../models/livros');
+const { readersModel } = require('../models/leitores');
+const { booksModel } = require('../models/livros');
 
-const registerNewReader = (req, res) => {  
-  const newReader = new leitoresModel({
-    nome: req.body.nome,
-    email: req.body.email,
-    senha: req.body.senha,
-    livros: req.body.livros.map(livro => {
-      const book = new livrosModel({
-        titulo: livro.titulo,
-        autoria: livro.autoria,
-        disponivel: livro.disponivel,
-        bairro: livro.bairro
-      });
-
-      book.save(err => {
-        if (err) {
-          return res.status(424).send({ message: err.message });
-        };        
-      }); 
-      return book;     
-    })
-  });   
-
-  newReader.save(err => {
+const registerNewReader = (req, res) => {
+  const newReader = new readersModel(req.body);
+  newReader.livros.map(livro => {
+    const newBook = new booksModel(livro);
+    newBook.save(err => {
+      if (err) {
+        return res.status(500).send({ message: err.message });
+      };
+    });    
+  }); 
+  
+  newReader.save((err) => {
     if (err) {
-      return res.status(424).send({ message: err.message });
+      return res.status(500).send({ message: err.message });
     };
     return res.status(201).send(newReader);
-  });  
+  });
 };
 
 const getAllReaders = (req, res) => {
-  leitoresModel.find({}, { nome: 1, email: 1}, (err, leitores) => {
+  leitoresModel.find({}, { codLeitor: 1, nome: 1, email: 1 }, (err, leitores) => {
     if (err) {
       return res.status(424).send({ message: err.message });
-    };   
+    };
     return res.status(200).send(leitores);
-  });  
+  });
 };
 
 const updateReader = (req, res) => {
   const id = req.params.id;
-  leitoresModel.find({ _id: id }, (err, leitor) => {    
+  leitoresModel.find({ _id: id }, (err, leitor) => {
     if (!leitor) {
       return res.status(404).send('Leitor não encontrado');
     } else {
@@ -50,10 +39,10 @@ const updateReader = (req, res) => {
         if (err) {
           return res.status(424).send({ message: err.message });
         };
-        return res.status(200).send('Informações atualizadas com sucesso!');         
-      });      
-    };         
-  }); 
+        return res.status(200).send('Informações atualizadas com sucesso!');
+      });
+    };
+  });
 };
 
 const deleteReader = (req, res) => {
