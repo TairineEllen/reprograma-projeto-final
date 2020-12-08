38043833a33319fc45c63c528f9d40e710b48ad9
TairineEllen <tairine.ellen@gmail.com>
2020-12-07 23:26:23 -0300

    Função registerNewBook refatorada - POST/leitores/id/livros

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -17,58 +17,50 @@ const getBooksByReader = (req, res) => {
       return res.status(500).send({ message: err.message });
     };
     if (!reader) {
-      return res.status(500).send('Leitor não encontrado');
+      return res.status(500).send('Leitor(a) não encontrado(a)');
     }
-    if (reader.livros.length > 0) {
-      return res.status(200).send(reader.livros);
+    if (!reader.livros.length) {
+      return res.status(404).send('Esse(a) leitor(a) ainda não possui livros cadastrados');
     } else {
-      return res.status(404).send('Esse leitor ainda não possui livros cadastrados');
-    };    
+      return res.status(200).send(reader.livros);
+    };
   });
 };
 
-// const getAllBooks = (req, res) => {
-//   const parametros = req.query;
-//   const bairro = req.query.bairro;
-//   const disponivel = req.query.disponivel;
-//   console.log(parametros)
-//   console.log(Object.keys(parametros))
-//   console.log(Object.values(parametros))
-//   const bairro2 = new RegExp(`^${bairro}$`, 'i')
-//   console.log(bairro2)
-//   livrosModel.find({ bairro: new RegExp(`^${bairro}$`, 'i'), disponivel: disponivel }, (err, livros) => {
-//     res.send(livros)
-//   });
-  
-// };
-
 const getAvailableBooks = (req, res) => {
-  livrosModel.find({ disponivel: true }, (err, livros) => {
+  booksModel.find({ disponivel: false }, (err, books) => {
     if (err) {
       return res.status(424).send({ message: err.message });
-    }; 
-    if (!livros.length) {
+    };
+    if (!books.length) {
       return res.status(404).send('Não há livros disponíveis para empréstimo');
     } else {
-      return res.status(200).send(livros);
+      return res.status(200).send(books);
     };
   });
 };
 
 const registerNewBook = (req, res) => {
-  livrosModel.find((err, livros) => {
-    const newBook = new livrosModel({
-      codLivro: livros[livros.length - 1].codLivro + 1,
-      ...req.body
-    });
-    
+  const idReader = req.params.idReader;
+  const newBook = new booksModel(req.body);
+
   newBook.save(err => {
     if (err) {
-      return res.status(424).send({ message: err.message });
+      return res.status(500).send({ message: err.message });
     };
-    return res.status(201).send(newBook);
   });
-  })
+
+  readersModel.findById(idReader, (err, reader) => {
+    if (err) {
+      return res.status(500).send({ message: err.message });
+    };
+    if (!reader) {
+      return res.status(404).send('Leitor(a) não encontrado(a)')
+    } else {
+      reader.livros.push(newBook);
+      return res.status(201).send(reader.livros);
+    };
+  });
 };
 
 const updateBook = (req, res) => {
@@ -82,7 +74,7 @@ const updateBook = (req, res) => {
           return res.status(424).send({ message: err.message });
         };
         return res.status(200).send('Informações atualizadas com sucesso');
-      });      
+      });
     };
   });
 };
@@ -100,7 +92,7 @@ const updateLocationAndStatus = (req, res) => {
         return res.status(200).send('Status do livro atualizado com sucesso');
       });
     };
-  });  
+  });
 };
 
 const deleteBook = (req, res) => {
--- src/routes/leitoresRoute.js
+++ src/routes/leitoresRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/leitoresController');
-const { getBooksByReader } = require('../controllers/livrosController');
+const { getBooksByReader, registerNewBook } = require('../controllers/livrosController');
 
 
 router.post('/', controller.registerNewReader);
+router.post('/:idReader/livros', registerNewBook);
 router.get('/', controller.getAllReaders);
 router.get('/:idReader', controller.getReaderById);
 router.get('/:idReader/livros', getBooksByReader);
