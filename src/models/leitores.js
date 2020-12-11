354391f612c5b559962713b9be7888ad6de96627
TairineEllen <tairine.ellen@gmail.com>
2020-12-11 11:36:27 -0300

    Adicionada validação do email em models/leitores.js

--- src/models/leitores.js
+++ src/models/leitores.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-const { booksSchema } = require('./livros')
+const { booksSchema } = require('./livros');
+const { isEmail } = require('validator');
+
 
 const readersSchema = new mongoose.Schema({
-  nome: { type: String },
-  email: { type: String },
-  senha: { type: String },
+  nome: { type: String, required: true },
+  email: { type: String, required: true, validate: [ isEmail, 'Email inválido' ] },
+  senha: { type: String, required: true },
   livros: [booksSchema]
 }, {
   versionKey: false
